30439ecde502370345c38d8d700d565df9dc4a2c
jiangtao <kernel.digger@gmail.com>
2011-06-30 21:54:13 +0800

    container_of

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -642,6 +642,9 @@ static inline void ftrace_dump(enum ftrace_dump_mode oops_dump_mode) { }
  * @member:	the name of the member within the struct.
  *
  */
+/*
+取得@ptr的容器结构实例的指针
+*/
 #define container_of(ptr, type, member) ({			\
 	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
 	(type *)( (char *)__mptr - offsetof(type,member) );})
